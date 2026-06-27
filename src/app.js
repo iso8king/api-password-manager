6a46792005eaff8c60854c2773d3c4f6b9ac2eac
Ridho Firdaus <ridhofirdaus321@gmail.com>
2026-06-27 19:50:23 +0700

    new branch

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
 
-logger.info("App loaded in port 4993");
-web.listen(4993);
+logger.info("App runnig");
+// web.listen(4993);
 
 
 export default web;
